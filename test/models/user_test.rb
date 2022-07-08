3528fb93c08aa2a732b8fc9e743c29f714715954
Leandran <41592532+Leandran@users.noreply.github.com>
2022-07-08 20:58:04 +0200

    Update Notifications

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,8 +16,6 @@
 #
 # Indexes
 #
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_name                  (name)
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 require 'test_helper'
